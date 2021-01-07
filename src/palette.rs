ba88d2e5d15cadde3e75e00849e3bdd108864b6d
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-07 13:40:09 +1100

    Generate colours of different lightnesses instead of hardcoding consts

--- src/palette.rs
+++ src/palette.rs
@@ -19,37 +19,46 @@ pub(crate) const ZENBURN_BG_PLUS_1: Lazy<Oklch> = Lazy::new(|| oklch(0.42760777,
 pub(crate) const ZENBURN_BG_PLUS_2: Lazy<Oklch> = Lazy::new(|| oklch(0.4854972, 0.0, 0.0));
 pub(crate) const ZENBURN_BG_PLUS_3: Lazy<Oklch> = Lazy::new(|| oklch(0.5417056, 0.0, 0.0));
 
+pub(crate) struct LightnessLevel(pub(crate) u32);
+
+impl From<LightnessLevel> for f32 {
+    fn from(lightness_level: LightnessLevel) -> Self {
+        match lightness_level.0 {
+            0 => 0.65,
+            1 => 0.75,
+            2 => 0.8,
+            3 => 0.85,
+            4 => 0.9,
+            _ => unreachable!(),
+        }
+    }
+}
+
 const COLOR_CHROMA: f32 = 0.065;
 
-const COLOR_L_MINUS_2: f32 = 0.65;
-const COLOR_L_MINUS_1: f32 = 0.75;
-const COLOR_L: f32 = 0.8;
-const COLOR_L_PLUS_1: f32 = 0.85;
-const COLOR_L_PLUS_2: f32 = 0.9;
+macro_rules! def_color_fn {
+    ($name:ident, hue: $hue:literal) => {
+        pub(crate) fn $name(lightness_level: LightnessLevel) -> Oklch {
+            oklch(lightness_level.into(), COLOR_CHROMA, $hue)
+        }
+    };
+}
 
-const RED_HUE: f32 = 19.5;
-pub(crate) const ZENBURN_RED_MINUS_1: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L_MINUS_2, COLOR_CHROMA, RED_HUE));
-pub(crate) const ZENBURN_RED: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L, COLOR_CHROMA, RED_HUE));
+def_color_fn!(red, hue: 19.5);
+def_color_fn!(orange, hue: 55.112522);
+def_color_fn!(yellow, hue: 91.0);
+def_color_fn!(green, hue: 145.0);
+def_color_fn!(cyan, hue: 200.0);
 
-pub(crate) const ZENBURN_ORANGE: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L, COLOR_CHROMA, 55.112522));
+pub(crate) fn blue(lightness_level: LightnessLevel) -> Oklch {
+    let chroma = if lightness_level.0 == 4 {
+        COLOR_CHROMA * 0.7
+    } else {
+        COLOR_CHROMA
+    };
 
-const YELLOW_HUE: f32 = 91.0;
-pub(crate) const ZENBURN_YELLOW_MINUS_1: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L, COLOR_CHROMA, YELLOW_HUE));
-pub(crate) const ZENBURN_YELLOW: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L_PLUS_2, COLOR_CHROMA, YELLOW_HUE));
-
-const GREEN_HUE: f32 = 145.0;
-pub(crate) const ZENBURN_GREEN_MINUS_1: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L_MINUS_2, COLOR_CHROMA, GREEN_HUE));
-pub(crate) const ZENBURN_GREEN: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L, COLOR_CHROMA, GREEN_HUE));
-pub(crate) const ZENBURN_GREEN_PLUS_1: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L_PLUS_2, COLOR_CHROMA, GREEN_HUE));
-
-const CYAN_HUE: f32 = 200.0;
-pub(crate) const ZENBURN_CYAN: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L_MINUS_1, COLOR_CHROMA, CYAN_HUE));
-pub(crate) const ZENBURN_CYAN_PLUS_1: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L, COLOR_CHROMA, CYAN_HUE));
-pub(crate) const ZENBURN_CYAN_PLUS_2: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L_PLUS_1, COLOR_CHROMA, CYAN_HUE));
-
-const BLUE_HUE: f32 = 243.0;
-pub(crate) const ZENBURN_BLUE_MINUS_1: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L, COLOR_CHROMA, BLUE_HUE));
-pub(crate) const ZENBURN_BLUE: Lazy<Oklch> = Lazy::new(|| oklch(COLOR_L_PLUS_2, COLOR_CHROMA * 0.7, BLUE_HUE));
+    oklch(lightness_level.into(), chroma, 243.0)
+}
 
 fn oklch(l: f32, c: f32, h: f32) -> Oklch {
     Oklch {
